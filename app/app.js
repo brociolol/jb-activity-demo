07548e19be67247d5a0e78d45a06369db75da8f1
brociolol <118844723+brociolol@users.noreply.github.com>
2023-02-12 16:06:57 -0600

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const configJSON = require('/config/config-json');
+const
+
+port = process.env.PORT || 80;
+
+const app = express();
+
+app.post('/execute', function(req, res) => { 
+
+  console.log('executed');
+
+});
+
+app.get('/index.html', function(req, res) => {
+
+  return res.render('/html/index.html');
+  
+});
+
+app.get('/config.json', function(req, res) => {
+
+  return res.status(200).json(configJSON(req));
+  
+});
+
+app.post('/save', function(req, res) => {
+
+  return res.status(200).json({});
+  
+});
+
+app.post('/publish', function(req, res) => {
+
+  return res.status(200).json({});
+  
+});
+
+app.post('/validate', function(req, res) => {
+
+  return res.status(200).json({});
+  
+});
+
+app.post('/stop', function(req, res) => {
+
+  return res.status(200).json({});
+  
+});
+
+app.post('/stop', function(req, res) => {
+
+  return res.status(200).json({});
+  
+});
+
+
+app.listen(port);
+console.log(port);
